cb54de1a61321b6617e5ac759870b826f693e9e0
forman_kairo <37811935+aforman2@users.noreply.github.com>
2026-04-27 17:00:56 -0400

    Add files via upload

---
+++ Lib Mng Sys/LMS/User/Account.java
@@ -0,0 +1,11 @@
+package LMS.User;
+
+public interface Account {
+
+    // Takes in a username and password to be checked with an account in the system
+    public boolean authenticate(String user, String pass);
+
+    // Allows logged-in user to reset their password
+    public void resetPassword();
+
+}
---
+++ Lib Mng Sys/LMS/User/Loan.java
@@ -0,0 +1,53 @@
+package LMS.User;
+
+import java.util.Date;
+
+public class Loan {
+
+    // INITIAL VARS
+    private String loanID;
+    private Date checkoutDate;
+    private Date dueDate;
+
+    // CONSTRUCTOR
+    public Loan(String id, Date cD, Date dD) {
+        this.loanID = id;
+        this.checkoutDate = cD;
+        this.dueDate = dD;
+    }
+
+    // MAIN METHODS
+
+    public boolean isOverdue() {
+        if (checkoutDate.getTime() >= dueDate.getTime()) {
+            return true;
+        }
+        return false;
+    }
+
+    // GETS AND SETS
+    public String getLoanID() {
+        return loanID;
+    }
+
+    public void setLoanID(String l) {
+        loanID = l;
+    }
+
+    public Date getCheckouDate() {
+        return checkoutDate;
+    }
+
+    public void setCheckoutDate(Date cd) {
+        checkoutDate = cd;
+    }
+
+    public Date getDueDate() {
+        return dueDate;
+    }
+
+    public void setDueDate(Date dD) {
+        dueDate = dD;
+    }
+
+}
