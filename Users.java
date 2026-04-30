646d54419bb41b1e65324e71d379921d3a0eb183
forman_kairo <37811935+aforman2@users.noreply.github.com>
2026-04-29 20:24:37 -0400

    connect to mysql

---
+++ Users.java
@@ -0,0 +1,31 @@
+public abstract class Users {
+
+    private String firstName;
+    private String lastName;
+    private String uName;
+    private String userPassword;
+
+    public Users(String fName, String lName, String uName, String uPassword) {
+        this.firstName = fName;
+        this.lastName = lName;
+        this.uName = uName;
+        this.userPassword = uPassword;
+    }
+
+    public String getFirstName() {
+        return this.firstName;
+    }
+
+    public String getLastName() {
+        return this.lastName;
+    }
+
+    public String getUName() {
+        return this.uName;
+    }
+
+    public String getUPass() {
+        return this.userPassword;
+    }
+
+}
