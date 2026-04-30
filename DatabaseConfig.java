93843749a379e5f409bf676590ee432555f23330
jtownsend4305 <jtownsend4305@gmail.com>
2026-04-29 23:45:08 -0400

    NewPaltzLibrary->LoginUi and LibraryDashboard Updates

--- DatabaseConfig.java
+++ DatabaseConfig.java
@@ -4,10 +4,16 @@ import java.sql.SQLException;
 
 public class DatabaseConfig {
     private static final String URL = "jdbc:mysql://localhost:3306/newpaltzlibrary";
-    private static final String USER = "root"; // Your MySQL username
-    private static final String PASS = "Vonnitta#4305"; // Your MySQL password
+    private static final String USER = "root"; 
+    private static final String PASS = "Vonnitta#4305"; 
 
     public static Connection getConnection() throws SQLException {
+        try {
+            // This ensures the JAR is loaded before trying to connect
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            System.err.println("JDBC Driver not found!");
+        }
         return DriverManager.getConnection(URL, USER, PASS);
     }
 }
