2d982f7cd3b55cb7503041d1351161a352e65cc4
jtownsend4305 <jtownsend4305@gmail.com>
2026-05-15 17:42:11 -0400

    Update DatabaseConfig.java

--- edu/newpaltz/library/config/DatabaseConfig.java
+++ edu/newpaltz/library/config/DatabaseConfig.java
@@ -6,7 +6,7 @@ import java.sql.SQLException;
 public class DatabaseConfig {
     private static final String URL = "jdbc:mysql://localhost:3306/newpaltzlibrary";
     private static final String USER = "root"; 
-    private static final String PASS = "Vonnitta#4305"; 
+    private static final String PASS = "your_mysql_password"; 
 
     //allow the connection to be established with the database anywhere in the program
     public static Connection getConnection() throws SQLException {
